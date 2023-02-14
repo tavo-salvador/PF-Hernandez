93c55a322cb30b2892bb699c7dc1faf657b7dcb5
Gustavo Hernandez <gustavo.hernandez@kint.com.mx>
2023-02-14 11:53:45 -0600

    update

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -4,15 +4,8 @@ import { BrowserModule } from '@angular/platform-browser';
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
-import { SharedModule } from './shared/shared.module';
-import { PagesModule } from './pages/pages.module';
-import { ServicesModule } from './services/services.module';
-import { NavToolbarComponent } from './layout/nav-toolbar/nav-toolbar.component';
-import { PageWrapperComponent } from './layout/page-wrapper/page-wrapper.component';
-import { AuthService } from './auth/services/auth-service/auth.service';
 import { HttpClientModule } from '@angular/common/http';
 import { DashboardModule } from './layout/dashboard.module';
-import { StoreModule } from '@ngrx/store';
 import { EffectsModule } from '@ngrx/effects';
 import { AppStoreModule } from './app-store.module';
 
@@ -28,6 +21,7 @@ import { AppStoreModule } from './app-store.module';
         HttpClientModule,
         BrowserAnimationsModule,
         DashboardModule,
+        AppStoreModule,
         EffectsModule.forRoot([]),
         
         
